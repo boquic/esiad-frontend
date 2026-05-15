751430ee90275d38b879aff48fa4cb46a64eb647
boquic <boris.quispe@upeu.edu.pe>
2026-05-14 19:00:15 -0500

    Refactor code structure for improved readability and maintainability

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('esiad-frontend');
+  protected readonly title = signal('SIGEPED');
 }
